b93451b0a9a96fe824cb0462c12fc330c150822a
chanmin01 <68585373+chanmin01@users.noreply.github.com>
2024-09-28 15:19:32 +0900

    AccountApplication

--- src/main/java/com/example/account/AccountApplication.java
+++ src/main/java/com/example/account/AccountApplication.java
@@ -1,4 +1,4 @@
-package org.example.account;
+package com.example.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
