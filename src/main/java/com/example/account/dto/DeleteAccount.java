4ccd3cc29a71d38b78fa992d1d6db4a3840484b8
chanmin01 <68585373+chanmin01@users.noreply.github.com>
2024-09-28 15:27:26 +0900

    delete account시 필요한 request, response

---
+++ src/main/java/com/example/account/dto/DeleteAccount.java
@@ -0,0 +1,46 @@
+package com.example.account.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+public class DeleteAccount {
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Request {
+        // 외부 클래스의 인스턴스와 관계없이 독립적으로 사용하기 위해 static으로 선언
+        @NotNull
+        @Min(1)
+        private Long userId;
+
+        @NotBlank
+        @Size(min = 10, max = 10)
+        private String accountNumber;
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class Response {
+        private Long userId;
+        private String accountNumber;
+        private LocalDateTime unRegisteredAt;
+
+        public static Response from(AccountDto accountDto) {
+            return Response.builder()
+                    .userId(accountDto.getUserId())
+                    .accountNumber(accountDto.getAccountNumber())
+                    .unRegisteredAt(accountDto.getUnRegisteredAt())
+                    .build();
+        }
+    }
+}
